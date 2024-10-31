9148677085d4e5b9a55a8c7ef292b5ec3297df93
jjuanzada <souza.victorjuan@gmail.com>
2024-10-31 14:49:07 -0300

    initial-setup

---
+++ exame-chuunin/src/main/java/com/grupo3/exame_chuunin/ExameChuuninApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.exame_chuunin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExameChuuninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExameChuuninApplication.class, args);
+	}
+
+}
---
+++ exame-chuunin/src/test/java/com/grupo3/exame_chuunin/ExameChuuninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.exame_chuunin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExameChuuninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
